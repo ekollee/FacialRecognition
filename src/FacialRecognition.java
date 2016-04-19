b84e845cd37d17bfd701d261fc85b8465e1effe8
Eric <erickollee@hotmail.com>
2016-04-19 17:36:34 -0400

    Initial commit

---
+++ src/FacialRecognition.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Eric on 4/19/2016.
+ */
+public class FacialRecognition {
+
+    public FacialRecognition (){
+
+    }
+
+    public static void main(String[] args) {
+        new FacialRecognition();
+    }
+
+}
