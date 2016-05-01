d9de68e4588795e8e9b7e4337b852ae216ccaddc
Graham Burgsma <graham.burgsma@gmail.com>
2016-05-01 14:54:34 -0400

    Added some stuff back in

--- src/FacialDetection.java
+++ src/FacialDetection.java
@@ -1,11 +1,5 @@
-import org.apache.commons.math3.linear.Array2DRowRealMatrix;
-import org.apache.commons.math3.linear.RealMatrix;
-
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,30 +12,41 @@ public class FacialDetection {
 
 
     public FacialDetection() {
-        List<EigenFace> faces = EigenFaceFactory.generateEigenFaces(10,100,"faces");
-        ImageProcessor imageProcessor = new ImageProcessor("testImage1.jpg");
-        GUIView.getInstance().setImage(imageProcessor.getImage());
+        List<EigenFace> faces = EigenFaceFactory.generateEigenFaces(10, 100, "jpgsmall");
 
+        ImageProcessor imageProcessor = new ImageProcessor("testImage2.jpg");
+        GUIView.getInstance().setImage(imageProcessor.getImage());
 
         imageProcessor.detectSkin();
 
-        ImageProcessor.saveImage(imageProcessor.getImage(), "neuralnet.jpg");
+        ImageProcessor.saveImage(imageProcessor.getImage(), "outImages/neuralnet.jpg");
         GUIView.getInstance().setImage(imageProcessor.getImage());
 
         imageProcessor.floodFillInit(Color.black.getRGB(), Color.white.getRGB());
-        ImageProcessor.saveImage(imageProcessor.getImage(), "blackfill.jpg");
+        ImageProcessor.saveImage(imageProcessor.getImage(), "outImages/blackfill.jpg");
 
         imageProcessor.fillHoles(Color.white.getRGB(), Color.black.getRGB());
-        ImageProcessor.saveImage(imageProcessor.getImage(), "whitefill.jpg");
+        ImageProcessor.saveImage(imageProcessor.getImage(), "outImages/whitefill.jpg");
 
         GUIView.getInstance().setImage(imageProcessor.getImage());
 
-//
+//        imageProcessor.overlayEdgeDetectionImage();
+//        ImageProcessor.saveImage(imageProcessor.getImage(), "outImages/edgeDetection.jpg");
+
         imageProcessor.findWhiteBlobDimensions();
-//
+
+        BufferedImage beforeImage = imageProcessor.getOriginalImageCopy();
+        imageProcessor.drawFaceBoxesOnImage(beforeImage);
+        ImageProcessor.saveImage(beforeImage, "outImages/beforeMerge.jpg");
+
+        imageProcessor.mergeFaces();
+
+        BufferedImage afterImage = imageProcessor.getOriginalImageCopy();
+        imageProcessor.drawFaceBoxesOnImage(afterImage);
+        ImageProcessor.saveImage(afterImage, "outImages/afterMerge.jpg");
+
         GUIView.getInstance().setImage(imageProcessor.getImage());
     }
-
 
 
     public static void main(String[] args) {
